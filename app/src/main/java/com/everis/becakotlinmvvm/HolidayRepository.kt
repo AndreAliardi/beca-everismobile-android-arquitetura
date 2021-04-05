b3999467c9bc0f2c275928320178d03b75eb127f
AndreAliardi <andre.aliardi@gmail.com>
2021-04-05 15:03:51 -0300

    :package: ApiInterface e RetrofitClient movidos para o package api

--- app/src/main/java/com/everis/becakotlinmvvm/HolidayRepository.kt
+++ app/src/main/java/com/everis/becakotlinmvvm/HolidayRepository.kt
@@ -2,6 +2,8 @@ package com.everis.becakotlinmvvm
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.everis.becakotlinmvvm.api.ApiInterface
+import com.everis.becakotlinmvvm.api.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
